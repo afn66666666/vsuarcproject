f6e51cc279b5833bde6d6b1ac92155f9e29bdd25
Vantus <avkuz@bk.ru>
2024-02-05 14:46:04 +0300

    Добавил подгрузку редактируемых данных-2

--- cardAppendForm.php
+++ cardAppendForm.php
@@ -61,7 +61,7 @@ else {
 <p><strong>КАРТОЧКА ЭКСКУРСИОННОГО ОБЪЕКТА</strong></p>
 <p>
 <label for="objectName">Наименование объекта</label>
-<input name="objectName" id="objectName" type="text" value="Предмет кремневый" />
+<input name="objectName" id="objectName" type="text" value="Предмет кремневый" required />
 </p>
 <p>
 <label for="objectCommonName">Обиходные названия*</label>
@@ -70,16 +70,16 @@ else {
 <p>Время возникновения, открытия объекта <input name="objectCreationTime" type="text" value="Эпоха камня (верхний палеолит)" /></p>
 <p>
 <label for="objectShortStory">Краткая история объекта</label>
-<textarea name="objectShortStory" id="objectShortStory" style="width: 50%;">Найден в 2005 г. при раскопках в музее-заповеднике &laquo;Костёнки&raquo; </textarea></p>
+<textarea name="objectShortStory" id="objectShortStory" style="width: 50%;" required >Найден в 2005 г. при раскопках в музее-заповеднике &laquo;Костёнки&raquo; </textarea></p>
 <p>
 <label for="objectShortStory">Внешние признаки (особенности стиля, общая характеристика, сохранность)</label>
-<textarea name="objectFeatures" id="objectFeatures" style="width: 50%;">Камень (светло-серый валунный кремень), Ретушь. Без видимых повреждений.</textarea></p>
+<textarea name="objectFeatures" id="objectFeatures" style="width: 50%;" required >Камень (светло-серый валунный кремень), Ретушь. Без видимых повреждений.</textarea></p>
 <p>
 <label for="objectAuthorsData">Данные об авторах объекта</label>
 <input name="objectAuthorsData" id="objectAuthorsData" type="text" value="Нет данных" style="width: 50%;"/></p>
 <p>Источники сведений об объекте <input name="objectInformationSource" type="text" value="Археологические раскопки" style="width: 50%;"/></p>
 <p>Фотографии или видеоматериалы <input name="objectMedia" type="file" value="" multiple/></p>
-<p>Дата составления карточки <input name="objectCardDate" type="date" value="" /></p>
+<p>Дата составления карточки <input name="objectCardDate" type="date" value="" required /></p>
 <p style="text-align: center;"><input name="objectSubmit" type="submit" value="Записать карточку" onclick="checkDate()"/></p>
 </form>
 <p>* данная строка используется при необходимости</p>
