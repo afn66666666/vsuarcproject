6b5f4a309178b21ccefe597f63ec74fbca42bb2d
Vantus <avkuz@bk.ru>
2024-02-06 17:52:29 +0300

    Добавил аутентификацию 1

--- authentification.php
+++ authentification.php
@@ -7,6 +7,16 @@ require 'designFile.php';
 $inputLogin = $_POST['username'];
 $inputPassword = $_POST['password'];
 
+$conn = pg_connect("host=pg3.sweb.ru port=5432 dbname=avkuzbkru user=".$inputLogin." password=".$inputPassword);
+if (!$conn) {
+	echo "connection error occurred.\n";
+	exit;
+}
+else {
+	echo "Подключение к БД успешно.\n";
+}
+
+
 if($inputLogin == $login && $inputPassword == $password){
 	unset($_session['inputerror']);
 	header('Location:cardMenu.html');
