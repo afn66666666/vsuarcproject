f53e35181e373aa50a26652458fe090a3301d6b1
Vantus <avkuz@bk.ru>
2024-02-06 18:11:15 +0300

    Добавил аутентификацию 2

--- authentification.php
+++ authentification.php
@@ -10,23 +10,31 @@ $inputPassword = $_POST['password'];
 $conn = pg_connect("host=pg3.sweb.ru port=5432 dbname=avkuzbkru user=".$inputLogin." password=".$inputPassword);
 if (!$conn) {
 	echo "connection error occurred.\n";
+	$_SESSION['inputError'] = 'Неправильный логин/пароль.';
+	header('Location: authentificationForm.php');
 	exit;
 }
 else {
 	echo "Подключение к БД успешно.\n";
-}
-
-
-if($inputLogin == $login && $inputPassword == $password){
+	$_SESSION['inputLogin'] = $inputLogin;
+	$_SESSION['inputPassword'] = $inputPassword;
 	unset($_session['inputerror']);
 	header('Location:cardMenu.html');
 	die();
+	pg_close();
 }
-else{
-	$_SESSION['inputError'] = 'Неправильный логин/пароль.';
-	 header('Location: authentificationForm.php');
-	 exit();
-}
+
+
+//if($inputLogin == $login && $inputPassword == $password){
+//	unset($_session['inputerror']);
+//	header('Location:cardMenu.html');
+//	die();
+//}
+//else{
+//	$_SESSION['inputError'] = 'Неправильный логин/пароль.';
+//	 header('Location: authentificationForm.php');
+//	 exit();
+//}
 
 
 ?>
